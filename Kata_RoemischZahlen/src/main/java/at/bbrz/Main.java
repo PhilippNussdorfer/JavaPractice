50ed7aad4c95c0272cde4fed3a5f5c30f4412689
Philipp <Philipp.nuss@gmx.at>
2024-01-26 10:26:03 +0100

    Refactoring

--- Kata_RoemischZahlen/src/main/java/at/bbrz/Main.java
+++ Kata_RoemischZahlen/src/main/java/at/bbrz/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var myRomanNumber = "MMMCMXCIX";
         var myArabianNumber = 1571;
-        var calculator = new SwitchBetweenArabianAndRomanNumberSystem();
+        var calculator = new SwitchBetweenArabianAndRomanNumberSystem(new RomanNumberChecker());
 
         System.out.println(calculator.getRomanNumber(myArabianNumber));
         System.out.println(calculator.getArabianNumber(myRomanNumber));
