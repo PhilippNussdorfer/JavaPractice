bdbd22eac555baeab46b5747cd376ed88c494672
Philipp <Philipp.nuss@gmx.at>
2024-01-30 08:11:55 +0100

    Refactored

--- Kata_RoemischZahlen/src/main/java/at/bbrz/Main.java
+++ Kata_RoemischZahlen/src/main/java/at/bbrz/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         var myRomanNumber = "MMMCMXCIX";
         var myArabianNumber = 1571;
-        var calculator = new SwitchBetweenArabianAndRomanNumberSystem(new RomanNumberChecker());
+        var calculator = new SwitchBetweenArabianAndRomanNumberSystem();
 
         System.out.println(calculator.getRomanNumber(myArabianNumber));
         System.out.println(calculator.getArabianNumber(myRomanNumber));
