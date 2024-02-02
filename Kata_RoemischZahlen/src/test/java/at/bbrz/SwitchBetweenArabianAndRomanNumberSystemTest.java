cd96c03ccf8e7c466f644709c25c70a8844333ac
Philipp <Philipp.nuss@gmx.at>
2024-02-02 08:31:18 +0100

    Refactored

--- Kata_RoemischZahlen/src/test/java/at/bbrz/SwitchBetweenArabianAndRomanNumberSystemTest.java
+++ Kata_RoemischZahlen/src/test/java/at/bbrz/SwitchBetweenArabianAndRomanNumberSystemTest.java
@@ -3,7 +3,6 @@ package at.bbrz;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class SwitchBetweenArabianAndRomanNumberSystemTest {
 
-    @Mock
     SwitchBetweenArabianAndRomanNumberSystem switcher;
 
     @BeforeEach
