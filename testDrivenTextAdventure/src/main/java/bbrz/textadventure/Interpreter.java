09821f54b25b0c0e5afcf85b0b15c0b30b568885
Philipp <Philipp.nuss@gmx.at>
2024-02-02 10:47:08 +0100

    Interpreter interpret's each action and if the command does not exist than it throws an CommandNotFoundException.

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/Interpreter.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/Interpreter.java
@@ -1,4 +1,32 @@
 package bbrz.textadventure;
 
+import bbrz.textadventure.actions.Action;
+import bbrz.textadventure.customException.CommandNotFoundException;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
 public class Interpreter {
+    private List<Action> actionList = new ArrayList<>();
+
+    public void addActions(Action ... actions) {
+        for (Action action : actions) {
+            actionList.add(action);
+        }
+    }
+
+    public void interpret(String commandWithParams) throws CommandNotFoundException {
+        String[] words = commandWithParams.toLowerCase().split(" ");
+
+        for (Action action : actionList) {
+            if (action.canHandle(words[0])) {
+                action.execute(words);
+                return;
+            }
+        }
+
+        throw new CommandNotFoundException("No such Command found.");
+    }
 }
