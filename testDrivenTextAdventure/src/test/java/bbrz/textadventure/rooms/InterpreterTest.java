21e9404064069f69214807b60b2d0b094e2cc8cf
Philipp <Philipp.nuss@gmx.at>
2024-02-02 10:49:28 +0100

    Unit Tests for interpreter with Mockito for Single word Commands and the custom Exception

--- testDrivenTextAdventure/src/test/java/bbrz/textadventure/rooms/InterpreterTest.java
+++ testDrivenTextAdventure/src/test/java/bbrz/textadventure/rooms/InterpreterTest.java
@@ -1,37 +1,66 @@
 package bbrz.textadventure.rooms;
 
 import bbrz.textadventure.Interpreter;
+import bbrz.textadventure.actions.Action;
+import bbrz.textadventure.customException.CommandNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class InterpreterTest {
 
-    Interpreter interpreter = new Interpreter();
+    @Mock
+    Action exitAction;
+    @Mock
+    Action moveAction;
+
+    Interpreter interpreter;
 
     @BeforeEach
     void setUp() {
-
+        interpreter = new Interpreter();
+        interpreter.addActions(exitAction, moveAction);
     }
 
     @Test
-    void moveRooms() {
-        var nextRoom = interpreter.interpretRoom("S");
-        assertTrue(nextRoom instanceof Clearing);
+    void interpretSingleWordAction() throws CommandNotFoundException {
 
-        nextRoom = interpreter.interpret("d");
-        assertTrue(nextRoom instanceof );
-
-        nextRoom = interpreter.interpret("a");
-        assertEquals("Clearing", nextRoom.name);
+        Mockito.when(exitAction.canHandle("exit")).thenReturn(true);
+        interpreter.interpret("exit");
+        Mockito.verify(exitAction, Mockito.times(1)).execute("exit");
     }
 
     @Test
-    void noRoomToGo() {
-        assertThrows(IllegalArgumentException.class, ()-> interpreter.interpret("W"));
+    void singleWordActionWithMorePossibleCommands() throws CommandNotFoundException {
+
+        Mockito.when(moveAction.canHandle("north")).thenReturn(true);
+        Mockito.when(moveAction.canHandle("south")).thenReturn(true);
+        Mockito.when(moveAction.canHandle("west")).thenReturn(true);
+        Mockito.when(moveAction.canHandle("east")).thenReturn(true);
+
+        interpreter.interpret("south");
+        Mockito.verify(moveAction, Mockito.times(1)).execute("south");
+
+        interpreter.interpret("north");
+        Mockito.verify(moveAction, Mockito.times(1)).execute("north");
+
+        interpreter.interpret("east");
+        Mockito.verify(moveAction, Mockito.times(1)).execute("east");
+
+        interpreter.interpret("west");
+        Mockito.verify(moveAction, Mockito.times(1)).execute("west");
+    }
+
+    @Test
+    void commandDoseNotExist() {
+        assertThrows(CommandNotFoundException.class, ()-> interpreter.interpret("Hello"));
     }
 }
