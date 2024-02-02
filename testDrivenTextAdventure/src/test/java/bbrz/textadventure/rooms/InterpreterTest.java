02a0ffb75f1a2949d57294a5e74588b573e06e9a
Philipp <Philipp.nuss@gmx.at>
2024-02-02 10:59:27 +0100

    Refactoring

--- testDrivenTextAdventure/src/test/java/bbrz/textadventure/rooms/InterpreterTest.java
+++ testDrivenTextAdventure/src/test/java/bbrz/textadventure/rooms/InterpreterTest.java
@@ -10,8 +10,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
