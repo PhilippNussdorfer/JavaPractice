7f80c1853310c364e2f75fa65b01f1160e6d3a7c
Philipp <Philipp.nuss@gmx.at>
2024-02-16 09:07:01 +0100

    added a try catch for RoomNotFoundException

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/Game.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/Game.java
@@ -1,17 +1,28 @@
 package bbrz.textadventure;
 
+import bbrz.textadventure.customException.RoomNotFoundException;
 import bbrz.textadventure.rooms.Room;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class Game {
-    private Interpreter interpreter;
-    private Player player;
+    private final Interpreter interpreter;
+    private final Player player;
     private Room currentRoom;
 
+    public Game(Interpreter interpreter, Player player, Room currentRoom) {
+        this.interpreter = interpreter;
+        this.player = player;
+        this.currentRoom = currentRoom;
+    }
+
     public void move(String direction) {
-        currentRoom = currentRoom.getRoom(direction);
+        try {
+            currentRoom = currentRoom.getRoom(direction);
+
+        } catch (RoomNotFoundException roomNotFound) {
+            System.out.println(roomNotFound.getMessage());
+
+        }
     }
 }
