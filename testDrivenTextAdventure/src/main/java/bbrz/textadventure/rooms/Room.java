4ce5e3416f0d3538a3099023ce7dcb475fcfa2cf
Philipp <Philipp.nuss@gmx.at>
2024-02-16 09:13:12 +0100

    removed abstract added som variables, getters and finished getRoom;

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/rooms/Room.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/rooms/Room.java
@@ -1,7 +1,24 @@
 package bbrz.textadventure.rooms;
 
-public abstract class Room {
-    public Room getRoom(String direction) {
-        return null;
+import bbrz.textadventure.customException.RoomNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class Room {
+
+    @Getter
+    private final String name;
+    @Getter
+    private final String description;
+    private final RoomPointer[] pointers;
+
+    public Room getRoom(String direction) throws RoomNotFoundException {
+        for (RoomPointer pointer : pointers) {
+            if (pointer.isDirection(direction)) {
+                return pointer.getTarget();
+            }
+        }
+        throw new RoomNotFoundException("There is no room in this direction.");
     }
 }
