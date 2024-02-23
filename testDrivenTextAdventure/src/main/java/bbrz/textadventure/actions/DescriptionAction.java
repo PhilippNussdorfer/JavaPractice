262f070cf468176a56403d720ed6a95e53d3cf69
Philipp <Philipp.nuss@gmx.at>
2024-02-23 11:08:20 +0100

    Implemented the DescriptionAction Items need to be implemented to use the item command

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/DescriptionAction.java
@@ -0,0 +1,44 @@
+package bbrz.textadventure.actions;
+
+import bbrz.textadventure.Game;
+import bbrz.textadventure.OutputWrapper;
+import bbrz.textadventure.customException.CommandNotFoundException;
+import jdk.jshell.spi.ExecutionControl;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+public class DescriptionAction extends AbAction {
+
+    private final List<String> additionList = List.of("location", "locationName", "item");
+    OutputWrapper wrapper;
+    public DescriptionAction(Game game, OutputWrapper wrapper, String ... possibleCommands) {
+        super(possibleCommands, game);
+        this.wrapper = wrapper;
+    }
+
+    @Override
+    public void execute(String... params) throws ExecutionControl.NotImplementedException, CommandNotFoundException {
+        if (params.length > 1) {
+            if (params[1].equalsIgnoreCase(additionList.get(0))) {
+                wrapper.outPrintln(game.getCurrentLocation().getDescription());
+            }
+
+            else if (params[1].equalsIgnoreCase(additionList.get(1))) {
+                wrapper.outPrintln(game.getCurrentLocation().getName());
+            }
+
+            else if (params.length > 2 && params[1].equalsIgnoreCase(additionList.get(2))) {
+                throw new ExecutionControl.NotImplementedException("please Implement item");
+            }
+
+            else {
+                throw new CommandNotFoundException("This is an command with errors: " + String.join(" ", Arrays.stream(params).toList()));
+            }
+        } else {
+            wrapper.outPrintln(game.getCurrentLocation().getDescription());
+        }
+    }
+}
