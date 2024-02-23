73b543d06f38b8dbd396a28e31d107215c27e156
Philipp <Philipp.nuss@gmx.at>
2024-02-23 11:01:25 +0100

    Wrapper to make outputs Testable

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/OutputWrapper.java
@@ -0,0 +1,12 @@
+package bbrz.textadventure;
+
+public class OutputWrapper {
+
+    public void outPrintln(String msg) {
+        System.out.println(msg);
+    }
+
+    public void outPrint(String msg) {
+        System.out.print(msg);
+    }
+}
