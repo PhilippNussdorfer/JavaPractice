5367f455a2dd5f62535fe589332277f221f75fda
Philipp <Philipp.nuss@gmx.at>
2024-02-23 11:02:24 +0100

    Added loopGame with setter for the ExitAction

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/Game.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/Game.java
@@ -3,11 +3,14 @@ package bbrz.textadventure;
 import bbrz.textadventure.customException.RoomNotFoundException;
 import bbrz.textadventure.rooms.Location;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class Game {
     private final Player player;
     private Location currentLocation;
+    @Setter
+    private boolean loopGame = true;
 
     public Game(Player player, Location currentLocation) {
         this.player = player;
