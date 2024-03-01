b941a7f77c8f4eaa48dd56ea50126b9f5deca267
Philipp <Philipp.nuss@gmx.at>
2024-03-01 08:38:32 +0100

    Added an enum to create colored text.

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/colors/TextColor.java
@@ -0,0 +1,18 @@
+package bbrz.textadventure.colors;
+
+import lombok.Getter;
+
+public enum TextColor {
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    DARK_BROWN("\u001B[33m"),
+    MAGENTA("\u001B[95m"),
+    BLUE("\u001B[34m"),
+    RESET("\u001B[0m");
+
+    @Getter
+    private final String value;
+    TextColor(String value) {
+        this.value = value;
+    }
+}
