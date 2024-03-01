7488bc3e1d3b9b8e44235c6826822f5135582bf7
Philipp <Philipp.nuss@gmx.at>
2024-03-01 08:39:18 +0100

    added error and colored wrapper.

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/OutputWrapper.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/OutputWrapper.java
@@ -1,5 +1,7 @@
 package bbrz.textadventure;
 
+import bbrz.textadventure.colors.TextColor;
+
 public class OutputWrapper {
 
     public void outPrintln(String msg) {
@@ -8,5 +10,17 @@ public class OutputWrapper {
 
     public void outPrint(String msg) {
         System.out.print(msg);
+    }
+
+    public void outErr(String msg) {
+        System.out.println(TextColor.RED.getValue() + msg + TextColor.RESET.getValue());
+    }
+
+    public void outPrintColored(String msg, TextColor color) {
+        System.out.print(color.getValue() + msg + TextColor.RESET.getValue());
+    }
+
+    public void outPrintlnColored(String msg, TextColor color) {
+        System.out.println(color.getValue() + msg + TextColor.RESET.getValue());
     }
 }
