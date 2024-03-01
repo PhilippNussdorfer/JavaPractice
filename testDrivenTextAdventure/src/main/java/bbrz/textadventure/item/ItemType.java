c91ea114864f44e49ad8a528acceefeb59811a43
Philipp <Philipp.nuss@gmx.at>
2024-03-01 11:11:04 +0100

    Enum for the itemTypes

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/item/ItemType.java
@@ -0,0 +1,13 @@
+package bbrz.textadventure.item;
+
+public enum ItemType {
+    HELMET,
+    BREASTPLATE,
+    ARMORED_PANTS,
+    BOOTS,
+    GAUNTLETS,
+    WEAPON,
+    ARTIFACT,
+    SHIELD,
+    MISC
+}
