22b08ca1e071cd943a2687db8dcfe7f83c893277
Philipp <Philipp.nuss@gmx.at>
2024-03-04 12:06:34 +0100

    Added getter

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/Action.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/Action.java
@@ -1,10 +1,16 @@
 package bbrz.textadventure.actions;
 
 import bbrz.textadventure.customException.CommandNotFoundException;
+import bbrz.textadventure.customException.NoFreeSpaceException;
+import bbrz.textadventure.customException.NoItemFoundException;
 import jdk.jshell.spi.ExecutionControl;
 
 public interface Action {
     boolean canHandle(String command);
 
-    void execute(String ... params) throws ExecutionControl.NotImplementedException, CommandNotFoundException;
+    void execute(String ... params) throws ExecutionControl.NotImplementedException, CommandNotFoundException, NoFreeSpaceException, NoItemFoundException;
+
+    String getName();
+    String getDescription();
+    String[] getPossibleCommands();
 }
