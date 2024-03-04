f4df1ee8647fd7e370836676190654a86a81bfbd
Philipp <Philipp.nuss@gmx.at>
2024-03-04 11:53:58 +0100

    Tests for PickUpAction

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/PickUpAction.java
@@ -0,0 +1,27 @@
+package bbrz.textadventure.actions;
+
+import bbrz.textadventure.Game;
+import bbrz.textadventure.customException.NoFreeSpaceException;
+import bbrz.textadventure.item.Item;
+
+public class PickUpAction extends AbAction {
+    public PickUpAction(Game game, String ... possibleCommands) {
+        super(game, "Pick up", "To pick up an item in an location <Command> <Item name>", possibleCommands);
+    }
+
+    @Override
+    public void execute(String... params) throws NoFreeSpaceException {
+        if (params.length <= 1) {
+            throw new IllegalArgumentException("An item name is needed to pickup an item!");
+        } else {
+            String itemName = params[1];
+
+            for (Item item : this.game.getCurrentLocation().getItems()) {
+                if (item.getName().equalsIgnoreCase(itemName)) {
+                    this.game.getPlayer().BPAddItems(item);
+                    this.game.getCurrentLocation().removeItems(item);
+                }
+            }
+        }
+    }
+}
