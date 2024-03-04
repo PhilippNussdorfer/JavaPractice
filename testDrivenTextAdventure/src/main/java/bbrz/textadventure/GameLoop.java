4927316b7eecceb2d6c11037ad48cf2d924b202e
Philipp <Philipp.nuss@gmx.at>
2024-03-04 13:06:20 +0100

    Added some Items

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/GameLoop.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/GameLoop.java
@@ -6,6 +6,9 @@ import bbrz.textadventure.customException.NoItemFoundException;
 import bbrz.textadventure.entity.Player;
 import bbrz.textadventure.colors.TextColor;
 import bbrz.textadventure.customException.CommandNotFoundException;
+import bbrz.textadventure.item.Item;
+import bbrz.textadventure.item.ItemStats;
+import bbrz.textadventure.item.ItemType;
 import bbrz.textadventure.rooms.Location;
 import bbrz.textadventure.rooms.LocationPointer;
 import jdk.jshell.spi.ExecutionControl;
@@ -49,6 +52,11 @@ public class GameLoop {
     }
 
     private void initGame() {
+        Item candle = Item.builder().type(ItemType.CONSUMABLE).description("When lit the world around you will be illuminated").stats(new ItemStats()).name("Candle").value(10).build();
+        Item matches = Item.builder().name("Matches").description("Can start a fire.").value(6).stats(new ItemStats()).type(ItemType.CONSUMABLE).build();
+        Item oldIronHelmet = Item.builder().name("Old Iron Helmet").description("It's olb butt still protects.").value(15).stats(new ItemStats(3, 5, 0)).type(ItemType.HELMET).build();
+        Item oldBronzeSword = Item.builder().name("Old Bronze Sword").description("when sharpened and repaired than it cold be like new").value(11).type(ItemType.WEAPON).stats(new ItemStats(0, 0, 3)).build();
+
         Location cottage = new Location("Cottage", "It's a small cottage where you can hear the wind howling through the cracks in the old windows and a small fireplace in the middle of the room where a fire is lit.");
         Location well = new Location("Well", "A Well with clear Water.");
         Location woods = new Location("Woods", "This dark and eerie where you can here the howling of the wind and owls through this dark night.");
@@ -57,12 +65,15 @@ public class GameLoop {
         Location beach = new Location("Beach", "On this sand beach where the sea slowly caresses the beach.");
         Location lake = new Location("Lake", "It's a small lake separated from the sea and where the clear blue water that reflects the moon.");
 
+        cottage.addItems(candle, matches);
         cottage.addPointers(new LocationPointer("s", well));
 
+        well.addItems(oldIronHelmet);
         well.addPointers(new LocationPointer("n", cottage),
                 new LocationPointer("s", woods),
                 new LocationPointer("w", cliff));
 
+        cliff.addItems(oldBronzeSword);
         cliff.addPointers(new LocationPointer("e", well),
                 new LocationPointer("s", beach));
 
