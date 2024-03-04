9f4f8bdc5a83acd95cefcc9401e54c7dd09a6791
Philipp <Philipp.nuss@gmx.at>
2024-03-04 13:05:57 +0100

    Refactored and added second constructor

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/item/ItemStats.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/item/ItemStats.java
@@ -1,14 +1,23 @@
 package bbrz.textadventure.item;
 
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
-@AllArgsConstructor
 public class ItemStats {
     private int hp;
     private int armor;
     private int dmg;
+
+    public ItemStats(int hp, int armor, int dmg) {
+        this.hp = hp;
+        this.armor = armor;
+        this.dmg = dmg;
+    }
+
+    public ItemStats() {
+        this.hp = 0;
+        this.armor = 0;
+        this.dmg = 0;
+    }
 
     /**
      * @return an List of integer in this order (hp, armor, dmg)
