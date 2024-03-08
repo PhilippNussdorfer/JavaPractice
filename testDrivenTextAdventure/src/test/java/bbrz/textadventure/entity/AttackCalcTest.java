5c67e87151e53ad179a47ae2469e8e0b44936909
Philipp <Philipp.nuss@gmx.at>
2024-03-08 12:45:46 +0100

    Tests for AttackCalc

---
+++ testDrivenTextAdventure/src/test/java/bbrz/textadventure/entity/AttackCalcTest.java
@@ -0,0 +1,36 @@
+package bbrz.textadventure.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttackCalcTest {
+
+    AttackCalc calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new AttackCalc();
+    }
+
+    @Test
+    void getsAttacked() {
+        int res = calc.getsAttacked(100, 10);
+        assertEquals(90, res);
+
+        res = calc.getsAttacked(100, 100);
+        assertEquals(1, res);
+    }
+
+    @Test
+    void getDmgRole() {
+        for (int i = 0; i < 10; i++) {
+            int res = calc.getDmgRole(5);
+            System.out.println(res);
+            assertTrue(List.of(3, 5, 4).contains(res));
+        }
+    }
+}
