1f06b60a4a04ee339ec17b125a435bf11c8287d9
Philipp <Philipp.nuss@gmx.at>
2024-03-18 13:48:13 +0100

    New class MapRuleMark to marks a location for the SelfMapCompositionGameLoader.

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/gameLoader/MapRuleMark.java
@@ -0,0 +1,14 @@
+package bbrz.textadventure.gameLoader;
+
+public enum MapRuleMark {
+    Woods,
+    Lake,
+    Beach,
+    Clearing,
+    Sea,
+    EdgeOfTheForest,
+    Meadow,
+    Swamp,
+    EdgeOfTheSwamp,
+    Well
+}
