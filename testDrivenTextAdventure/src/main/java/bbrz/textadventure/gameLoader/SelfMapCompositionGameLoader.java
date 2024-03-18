9364bbb8e12a3f32a27024ce972be14452efd217
Philipp <Philipp.nuss@gmx.at>
2024-03-18 13:49:44 +0100

    SelfMapCompositionGameLoader creates a game map based on specified rules!

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/gameLoader/SelfMapCompositionGameLoader.java
@@ -0,0 +1,41 @@
+package bbrz.textadventure.gameLoader;
+
+import bbrz.textadventure.Game;
+import bbrz.textadventure.colors.TextColor;
+import bbrz.textadventure.entity.AttackCalc;
+import bbrz.textadventure.entity.Player;
+import bbrz.textadventure.item.Backpack;
+import bbrz.textadventure.item.Equipped;
+import bbrz.textadventure.locatins.Location;
+import bbrz.textadventure.tools.OutputWrapper;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class SelfMapCompositionGameLoader implements GameLoader {
+
+    private List<Location> initLocations() {
+        return List.of(
+                new Location("Cottage", "It's a small cottage where you can hear the wind howling through the cracks in the old windows and a small fireplace in the middle of the room where a fire is lit."),
+                new Location("Well", "A Well with clear Water."),
+                new Location("Woods", "This dark and eerie where you can here the howling of the wind and owls through this dark night."),
+                new Location("Clearing", "A small clearing in the woods with a big rock in the middle surrounded with firefly's"),
+                new Location("Cliff", "On top on the cliff, when you look down you can see the beach and the sea where the moon reflects."),
+                new Location("Beach", "On this sand beach where the sea slowly caresses the beach."),
+                new Location("Lake", "It's a small lake separated from the sea and where the clear blue water that reflects the moon.")
+        );
+    }
+
+    @Override
+    public Game initGame(OutputWrapper wrapper, Scanner scanner) {
+        var locations = initLocations();
+
+        wrapper.outPrintColored("You are:\n>", TextColor.GREEN);
+        String name = scanner.nextLine();
+
+        Game game = new Game(new Player(name, 10, 0, 2, wrapper, new AttackCalc(), new Backpack(wrapper), new Equipped(wrapper)), locations.get(0), wrapper);
+        game.addInterpreter(InterpreterInit.init(game, wrapper));
+
+        return game;
+    }
+}
