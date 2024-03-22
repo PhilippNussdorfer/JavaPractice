99403437a65087acd8de3da7f84a3e5dffe72b12
Philipp <Philipp.nuss@gmx.at>
2024-03-22 12:44:45 +0100

    Refactoring

--- testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/DescriptionAction.java
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/DescriptionAction.java
@@ -12,9 +12,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DescriptionAction extends AbAction {
+    private final static String LOCATION = "location";
+    private final static String LOCATION_NAME = "locationName";
+    private final static String ITEM_NAME = "itemName";
 
     @Getter
-    private final List<String> additionList = List.of("location", "locationName", "item");
+    private final List<String> additionList = List.of(LOCATION, LOCATION_NAME, ITEM_NAME);
     OutputWrapper wrapper;
     public DescriptionAction(Game game, OutputWrapper wrapper, String ... possibleCommands) {
         super(game, "Describe", "To describe an location, item or get the location name <Command> <Addition> <Item name>", possibleCommands);
@@ -24,35 +27,44 @@ public class DescriptionAction extends AbAction {
     @Override
     public void execute(String... params) throws CommandNotFoundException, NoItemFoundException {
         if (params.length > 1) {
-            if (params[1].equalsIgnoreCase(additionList.get(0))) {
+            if (params[1].equalsIgnoreCase(LOCATION)) {
                 wrapper.outPrintlnColored(game.getCurrentLocation().getDescription() + "\n", TextColor.DARK_BROWN);
+                return;
             }
 
-            else if (params[1].equalsIgnoreCase(additionList.get(1))) {
+            if (params[1].equalsIgnoreCase(LOCATION_NAME)) {
                 wrapper.outPrintlnColored(game.getCurrentLocation().getName() + "\n", TextColor.DARK_BROWN);
+                return;
             }
 
-            else if (params.length > 2 && params[1].equalsIgnoreCase(additionList.get(2))) {
-                var locationItems = game.getCurrentLocation().getItems();
-                var playerBp = game.getPlayer().getBp().getBackpack();
-                var itemName = params[2];
-
-                var item = getItemFromString(locationItems, itemName);
-                if (item == null) {
-                    item = getItemFromString(playerBp, itemName);
-                    if (item == null) {
-                        throw new NoItemFoundException("Could not find the item with the name: " + itemName + "!");
-                    }
-                }
-                wrapper.outPrintlnColored(item.getDescription(), TextColor.DARK_BROWN);
+            if (params.length > 2 && params[1].equalsIgnoreCase(ITEM_NAME)) {
+                showItemDescription(params);
+                return;
             }
 
-            else {
-                throw new CommandNotFoundException("This is an command with errors: " + String.join(" ", Arrays.stream(params).toList()));
-            }
+            throw new CommandNotFoundException("This is an command with errors: " + String.join(" ", Arrays.stream(params).toList()));
         } else {
             wrapper.outPrintlnColored(game.getCurrentLocation().getDescription() + "\n", TextColor.DARK_BROWN);
         }
+    }
+
+    private void showItemDescription(String[] params) throws NoItemFoundException {
+        var locationItems = game.getCurrentLocation().getItems();
+        var playerBp = game.getPlayer().getBp().getBackpack();
+        var itemName = params[2];
+
+        var item = getItemFromString(locationItems, itemName);
+        if (item == null) {
+            item = getItemFromString(playerBp, itemName);
+            if (item == null) {
+                throw new NoItemFoundException("Could not find the item with the name: " + itemName + "!");
+            }
+        }
+        var hp = item.getStats().getItemStats().get(0);
+        var armor = item.getStats().getItemStats().get(1);
+        var dmg = item.getStats().getItemStats().get(2);
+
+        wrapper.outPrintlnColored(item.getDescription() + ", it is a: " + item.getType() + ", bonus stats Hp: " + hp + ", Armor: " + armor + ", Dmg: " + dmg, TextColor.DARK_BROWN);
     }
 
     private Item getItemFromString(List<Item> items, String name) {
