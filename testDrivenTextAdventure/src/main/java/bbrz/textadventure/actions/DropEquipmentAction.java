d5c376f7a5efd0b3dfd773fd67eca179496c7902
Philipp <Philipp.nuss@gmx.at>
2024-03-22 10:21:19 +0100

    Crated an action to drop the equipment and made test for it.

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/actions/DropEquipmentAction.java
@@ -0,0 +1,26 @@
+package bbrz.textadventure.actions;
+
+import bbrz.textadventure.Game;
+import bbrz.textadventure.customException.NoFreeSpaceException;
+import bbrz.textadventure.customException.NoItemFoundException;
+import bbrz.textadventure.item.Item;
+
+public class DropEquipmentAction extends AbAction {
+
+    public DropEquipmentAction(Game game, String ... possibleCommands) {
+        super(game, "Drop Equipment", "drops the item in the backpack or to the floor when the backpack is full <Command> <Item Name>", possibleCommands);
+    }
+
+    @Override
+    public void execute(String... params) throws NoItemFoundException, NoFreeSpaceException {
+        if (params.length > 1) {
+            for (Item item : game.getPlayer().getEquipped().getEquippedList()) {
+                if (item.getName().equalsIgnoreCase(params[1])) {
+                    game.getPlayer().dropEquipment(game, item);
+                }
+            }
+        } else {
+            throw new NoItemFoundException("Please enter an item name with the command or use help when you are not sure on how to use it.");
+        }
+    }
+}
---
+++ testDrivenTextAdventure/src/test/java/bbrz/textadventure/actions/DropEquipmentActionTest.java
@@ -0,0 +1,70 @@
+package bbrz.textadventure.actions;
+
+import bbrz.textadventure.Game;
+import bbrz.textadventure.customException.NoFreeSpaceException;
+import bbrz.textadventure.customException.NoItemFoundException;
+import bbrz.textadventure.entity.Player;
+import bbrz.textadventure.item.Equipped;
+import bbrz.textadventure.item.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class DropEquipmentActionTest {
+
+    DropEquipmentAction action;
+
+    @Mock
+    Game game;
+    @Mock
+    Player player;
+    @Mock
+    Equipped equipped;
+    @Mock
+    Item item;
+    @Mock
+    Item secItem;
+
+    @BeforeEach
+    void setUp() {
+        action = new DropEquipmentAction(game, "de", "drop-eq");
+    }
+
+    @Test
+    void execute() throws NoFreeSpaceException, NoItemFoundException {
+        Mockito.when(game.getPlayer()).thenReturn(player);
+        Mockito.when(player.getEquipped()).thenReturn(equipped);
+        Mockito.when(equipped.getEquippedList()).thenReturn(List.of(item, secItem));
+        Mockito.when(item.getName()).thenReturn("Wooden Round Shield");
+        Mockito.when(secItem.getName()).thenReturn("Ring");
+
+        action.execute("de", "ring");
+        Mockito.verify(player, Mockito.times(1)).dropEquipment(game, secItem);
+
+        action.execute("de", "none");
+        Mockito.verify(player, Mockito.times(0)).dropEquipment(game, item);
+
+        action.execute("drop-eq", "Wooden Round Shield");
+        Mockito.verify(player, Mockito.times(1)).dropEquipment(game, item);
+    }
+
+    @Test
+    void throwException() {
+        assertThrows(NoItemFoundException.class, ()->action.execute("eq"));
+    }
+
+    @Test
+    void canHandle() {
+        assertTrue(action.canHandle("drop-EQ"));
+        assertTrue(action.canHandle("DE"));
+        assertFalse(action.canHandle("exit"));
+    }
+}
