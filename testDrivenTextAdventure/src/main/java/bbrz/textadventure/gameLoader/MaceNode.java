088cf33dc4579fd67c82534283840aba343d1d8d
Philipp <Philipp.nuss@gmx.at>
2024-03-29 09:31:56 +0100

    MaceNode for the MaceGenerator

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/gameLoader/MaceNode.java
@@ -0,0 +1,11 @@
+package bbrz.textadventure.gameLoader;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MaceNode {
+    private final int x;
+    private final int y;
+}
