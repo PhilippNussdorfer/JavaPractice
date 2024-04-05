31822fc8650b19a010cbde9591c3e48ce465eacd
Philipp <Philipp.nuss@gmx.at>
2024-04-05 10:12:09 +0200

    finished the tests and the walker for the maze

--- testDrivenTextAdventure/src/test/java/bbrz/textadventure/gameLoader/MazeGeneratorTest.java
+++ testDrivenTextAdventure/src/test/java/bbrz/textadventure/gameLoader/MazeGeneratorTest.java
@@ -20,6 +20,8 @@ class MazeGeneratorTest {
 
     @Mock
     Location loc;
+    @Mock
+    Location marker;
 
     @BeforeEach
     void setUp() {
@@ -61,44 +63,86 @@ class MazeGeneratorTest {
         assertTrue(countNulls >= 40);
     }
 
+    @Test
+    void WalkThroughMaze() {
+        int walkableTiles = walker(gen.getMazeAsList(), 0, 0);
+        int LocationCount = 0;
+
+        for (List<Location> row : gen.getMazeAsList()) {
+            for (Location location : row) {
+                if (location != null) {
+                    LocationCount++;
+                }
+            }
+        }
+
+        assertEquals(LocationCount, walkableTiles);
+    }
+
     private int walker(List<List<Location>> maze, int x, int y) {
-        var currentLoc = maze.get(y).get(x);
-        List<Location> listOfDirections = new ArrayList();
-        int currentX = x, currentY = y, count = 0;
+        maze.get(y).set(x, marker);
 
-        if (currentY - 1 > 0) {
-            var up = maze.get(currentY - 1).get(currentX);
-            listOfDirections.add(up);
-        }
-        if (currentY + 1 < maze.size()) {
-            var down = maze.get(currentY + 1).get(currentX);
-            listOfDirections.add(down);
-        }
-        if (currentX - 1 > 0) {
-            var left = maze.get(currentY).get(currentX - 1);
-            listOfDirections.add(left);
-        }
-        if (currentX + 1 < maze.size()) {
-            var right = maze.get(currentY).get(currentX + 1);
-            listOfDirections.add(right);
-        }
+        List<Integer> listOf_XDir = new ArrayList();
+        List<Integer> listOf_YDir = new ArrayList();
 
-        Iterator<Location> iterator = listOfDirections.iterator();
+        int count = 1, countLocationOccurrence = 0, currentX = x, currentY = y;
+        boolean hasLocations = true;
 
-        while (iterator.hasNext()) {
-            Location next = iterator.next();
-            if (next == null) {
-                iterator.remove();
+        while (hasLocations) {
+            if (currentY - 1 >= 0) {
+                listOf_YDir.add(currentY - 1);
+                listOf_XDir.add(currentX);
+            }
+            if (currentY + 1 < maze.size()) {
+                listOf_YDir.add(currentY + 1);
+                listOf_XDir.add(currentX);
+            }
+
+            if (currentX - 1 >= 0) {
+                listOf_XDir.add(currentX - 1);
+                listOf_YDir.add(currentY);
+            }
+            if (currentX + 1 < maze.size()) {
+                listOf_XDir.add(currentX + 1);
+                listOf_YDir.add(currentY);
+            }
+
+            Iterator<Integer> xDirIterator = listOf_XDir.iterator();
+            Iterator<Integer> yDirIterator = listOf_YDir.iterator();
+
+            while (xDirIterator.hasNext() && yDirIterator.hasNext()) {
+                int xDir = xDirIterator.next(), yDir = yDirIterator.next();
+
+                if (maze.get(yDir).get(xDir) == loc) {
+                    countLocationOccurrence ++;
+                } else {
+                    xDirIterator.remove();
+                    yDirIterator.remove();
+                }
+            }
+
+            if (countLocationOccurrence > 1) {
+                for (int i = 0; i < countLocationOccurrence; i++) {
+                    count += walker(maze, listOf_XDir.get(i), listOf_YDir.get(i));
+                }
+            }
+
+            if (countLocationOccurrence == 1) {
+                currentX = listOf_XDir.get(0);
+                currentY = listOf_YDir.get(0);
+
+                maze.get(currentY).set(currentX, marker);
+                count ++;
+            }
+
+            if (countLocationOccurrence == 0) {
+                hasLocations = false;
+            } else {
+                countLocationOccurrence = 0;
             }
         }
 
-        if (listOfDirections.size() >= 2) {
-            for (Location loc : listOfDirections) {
-                walker(maze, x, y);
-            }
-        }
-
-        return 0;
+        return count;
     }
 
     private int countNewLines(String str) {
