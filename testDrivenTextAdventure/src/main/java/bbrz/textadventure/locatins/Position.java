f6f0fc03543bc7b7e92de913107c694df1300a73
Philipp <Philipp.nuss@gmx.at>
2024-04-05 11:23:39 +0200

    Added Position with x and y cords

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/locatins/Position.java
@@ -0,0 +1,10 @@
+package bbrz.textadventure.locatins;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Position {
+    private final int x, y;
+}
