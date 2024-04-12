00ddd2ebbba9728c75331c37e0873dc30331063b
Philipp <Philipp.nuss@gmx.at>
2024-04-12 08:32:11 +0200

    New Interpreter for the map generation

---
+++ testDrivenTextAdventure/src/main/java/bbrz/textadventure/rules/RuleInterpreter.java
@@ -0,0 +1,23 @@
+package bbrz.textadventure.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuleInterpreter {
+    private final List<Rule> rules = new ArrayList<>();
+
+    private void init() {
+        rules.addAll(List.of(
+            new StartingLocRule()
+        ));
+    }
+
+    public boolean interpretRule(MapRuleMark prevLocation, MapRuleMark randomLocation) {
+        for (Rule rule : rules) {
+            if (rule.canHandle(prevLocation)) {
+                return rule.isInRules(prevLocation, randomLocation);
+            }
+        }
+        return false;
+    }
+}
