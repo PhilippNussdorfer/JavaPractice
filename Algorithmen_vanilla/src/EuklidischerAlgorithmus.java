c0250a3574512516e3d1969bde1e2b24539fc2cb
Philipp <Philipp.nuss@gmx.at>
2024-05-08 14:13:25 +0200

    Math exercises

---
+++ Algorithmen_vanilla/src/EuklidischerAlgorithmus.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EuklidischerAlgorithmus {
+
+    public static void main(String[] args) {
+        System.out.println(ggt(224, 376));
+        System.out.println(kgv(224, 376));
+    }
+
+    private static int ggt(int firstNum, int secNum) {
+        int biggest = Math.max(firstNum, secNum);
+        int smallest = Math.min(firstNum, secNum);
+
+        List<Integer> rest = new ArrayList<>();
+        rest.add(biggest);
+        rest.add(smallest);
+
+        List<Integer> multiply = new ArrayList<>();
+        multiply.add(null);
+        multiply.add(null);
+
+        int count = 1;
+        for (int i = 0; ; i++) {
+            int first = rest.get(i);
+            int sec = rest.get(i + 1);
+
+            while (first - (sec * count) > 0) {
+                if (first - (sec * (count + 1)) == 0)
+                    return sec;
+                if (first - (sec * (count + 1)) < 0) {
+                    rest.add(first - (sec * count));
+                    multiply.add(count);
+                    count = 1;
+                    break;
+                } else {
+                    count ++;
+                }
+            }
+        }
+    }
+
+    private static int kgv(int firstNum, int secNum) {
+        int res = ggt(firstNum, secNum);
+        return (firstNum * secNum) / res;
+    }
+}
