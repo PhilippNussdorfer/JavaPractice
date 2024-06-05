1adc2175ec1298c9084b34f5185106afb501ae82
Philipp <Philipp.nuss@gmx.at>
2024-06-05 12:30:21 +0200

    Started with cell from the tutorial and added the address of the website

--- AStar_Algorithm/src/main/java/Cell.java
+++ AStar_Algorithm/src/main/java/Cell.java
@@ -1,2 +1,13 @@
+//https://www.geeksforgeeks.org/a-search-algorithm/
 public class Cell {
+    int parent_i, parent_j;
+    double f, g, h;
+
+    public Cell() {
+        this.parent_i = 0;
+        this.parent_j = 0;
+        this.f = 0;
+        this.g = 0;
+        this.h = 0;
+    }
 }
