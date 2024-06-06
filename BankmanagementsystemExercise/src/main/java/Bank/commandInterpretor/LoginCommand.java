6434070cb9e0f5bad888a974a3a296fed8466ed2
Philipp <Philipp.nuss@gmx.at>
2024-06-06 15:23:15 +0200

    finished LoginCommand

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/LoginCommand.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/LoginCommand.java
@@ -1,6 +1,10 @@
 package Bank.commandInterpretor;
 
 import Bank.Bundle;
+import Bank.person.Admin;
+import Bank.person.Customer;
+
+import java.util.Objects;
 
 public class LoginCommand extends CommandABS {
 
@@ -10,11 +14,35 @@ public class LoginCommand extends CommandABS {
 
     @Override
     public void execute(String[] params) {
+        try {
+            for (Customer customer : bundle.getCustomers()) {
+                var id = Long.getLong(params[1]);
 
+                if (Objects.equals(id, customer.getCustomerID())) {
+                    if (customer.login(params[2])) {
+                        bundle.setUser(customer);
+                    }
+                }
+            }
+
+            for (Admin admin : bundle.getAdmins()) {
+                var id = Long.getLong(params[1]);
+
+                if (Objects.equals(id, admin.getEmployeeID())) {
+                    if (admin.login(params[2])) {
+                        bundle.setUser(admin);
+                    }
+                }
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public String help() {
-        return "";
+        return formatter.formatStringLength(75, "Login with pin and ID") + formatter.formatStringLength(50, "<Command> <ID> <Pin>") + " | Commands: "
+                + formatter.concatStringArray(getCommands());
     }
 }
