b94406571258b27154039c0bb81038ddf2e45363
Philipp <Philipp.nuss@gmx.at>
2024-06-06 14:53:29 +0200

    Added formatStringLength to the StringFormatter

--- BankmanagementsystemExercise/src/main/java/Bank/tools/StringFormatter.java
+++ BankmanagementsystemExercise/src/main/java/Bank/tools/StringFormatter.java
@@ -4,4 +4,8 @@ public class StringFormatter {
     public String concatStringArray(String ... arr) {
         return String.join(", ", arr);
     }
+
+    public String formatStringLength(int length, String strToFormat) {
+        return String.format("%1$-" + length + "s", strToFormat);
+    }
 }
