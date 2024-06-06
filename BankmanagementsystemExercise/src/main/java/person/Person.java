173bae3655e79f12fda9daf9992aa4d501973403
Philipp <Philipp.nuss@gmx.at>
2024-06-06 11:42:28 +0200

    add pin to person and transferred the login methode

--- BankmanagementsystemExercise/src/main/java/person/Person.java
+++ BankmanagementsystemExercise/src/main/java/person/Person.java
@@ -9,4 +9,9 @@ public abstract class Person {
     private String name;
     private String birthDay;
     private Long socialNumber;
+    private String pin;
+
+    public boolean login(String pin) {
+        return pin.equals(this.pin);
+    }
 }
