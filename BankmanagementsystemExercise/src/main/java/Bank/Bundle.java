1d22f0a04f8397bb5bc609977c1e0400b6689073
Philipp <Philipp.nuss@gmx.at>
2024-06-06 14:27:11 +0200

    refactoring

--- BankmanagementsystemExercise/src/main/java/Bank/Bundle.java
+++ BankmanagementsystemExercise/src/main/java/Bank/Bundle.java
@@ -1,4 +1,20 @@
 package Bank;
 
+import Bank.commandInterpretor.Interpreter;
+import Bank.person.Admin;
+import Bank.person.Customer;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class Bundle {
+    private Interpreter interpreter = null;
+    private final List<Customer> customers = new ArrayList<>();
+    private final List<Admin> admins = new ArrayList<>();
+
+    public void addInterpreter(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
 }
