2a8d33314dcf136b2267512e03d109c1cf97e33a
Philipp <Philipp.nuss@gmx.at>
2024-06-06 14:18:55 +0200

    Added Interpreter

---
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/Interpreter.java
@@ -0,0 +1,29 @@
+package Bank.commandInterpretor;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Interpreter {
+    @Getter
+    private List<Command> commands = new ArrayList<>();
+
+    public void addCommand(Command ... commands) {
+        this.commands.addAll(Arrays.asList(commands));
+    }
+
+    public void interpret(String input) {
+        var params = input.split(" ");
+
+        for (Command command : commands) {
+            if (command.canHandle(params[0])) {
+                command.execute(params);
+                return;
+            }
+        }
+
+        commands.get(0).execute(params);
+    }
+}
