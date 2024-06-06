1313f535a69c96483525d9b10a64b24bda954fe9
Philipp <Philipp.nuss@gmx.at>
2024-06-06 15:00:11 +0200

    finished Add customer command

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/AddCustomerCommand.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/AddCustomerCommand.java
@@ -1,6 +1,8 @@
 package Bank.commandInterpretor;
 
 import Bank.Bundle;
+import Bank.person.Admin;
+import Bank.person.Customer;
 
 public class AddCustomerCommand extends CommandABS {
 
@@ -10,11 +12,18 @@ public class AddCustomerCommand extends CommandABS {
 
     @Override
     public void execute(String[] params) {
-
+        try {
+            if (bundle.getUser() instanceof Admin && params.length >= 6) {
+                bundle.getCustomers().add(new Customer(params[1], params[2], Long.getLong(params[3]), Long.getLong(params[4]), params[5]));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public String help() {
-        return null;
+        return formatter.formatStringLength(75, "Adds new customer " + formatter.formatStringLength(50, "<Command> <Name> <Birth Day> <Social Number> <ID> <pin>") + " | Commands: "
+                + formatter.concatStringArray(getCommands()));
     }
 }
