4d92e65041397a1b535c969871bcdf38ef841371
Philipp <Philipp.nuss@gmx.at>
2024-06-06 11:41:30 +0200

    Added Admin

---
+++ BankmanagementsystemExercise/src/main/java/person/Admin.java
@@ -0,0 +1,9 @@
+package person;
+
+public class Admin extends Person {
+    private Long employeeID;
+
+    public Admin(String name, String birthDay, Long socialNumber, String pin) {
+        super(name, birthDay, socialNumber, pin);
+    }
+}
