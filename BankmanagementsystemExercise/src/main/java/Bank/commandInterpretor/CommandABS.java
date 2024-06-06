e6057e1fc03f456a1d4379df731173586b0a39e3
Philipp <Philipp.nuss@gmx.at>
2024-06-06 14:51:21 +0200

    Added StringFormatter

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/CommandABS.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/CommandABS.java
@@ -1,6 +1,7 @@
 package Bank.commandInterpretor;
 
 import Bank.Bundle;
+import Bank.tools.StringFormatter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ public abstract class CommandABS implements Command {
     @Getter
     private final String[] commands;
     protected final Bundle bundle;
+    protected final StringFormatter formatter = new StringFormatter();
 
     @Override
     public boolean canHandle(String command) {
