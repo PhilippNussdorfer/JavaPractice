c336354b18a96ccef0827515fb80de4e15aef43f
Philipp <Philipp.nuss@gmx.at>
2024-06-06 14:34:11 +0200

    Added user and gave an setter

--- BankmanagementsystemExercise/src/main/java/Bank/Bundle.java
+++ BankmanagementsystemExercise/src/main/java/Bank/Bundle.java
@@ -3,7 +3,9 @@ package Bank;
 import Bank.commandInterpretor.Interpreter;
 import Bank.person.Admin;
 import Bank.person.Customer;
+import Bank.person.Person;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,8 @@ public class Bundle {
     private Interpreter interpreter = null;
     private final List<Customer> customers = new ArrayList<>();
     private final List<Admin> admins = new ArrayList<>();
+    @Setter
+    private Person user;
 
     public void addInterpreter(Interpreter interpreter) {
         this.interpreter = interpreter;
