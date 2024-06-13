edfb19c5a871cc8c69773d626061fde14a54f7d0
Philipp <Philipp.nuss@gmx.at>
2024-06-13 09:54:03 +0200

    Refactoring

--- BankmanagementsystemExercise/src/main/java/Bank/BankManagementSystem.java
+++ BankmanagementsystemExercise/src/main/java/Bank/BankManagementSystem.java
@@ -4,22 +4,23 @@ import Bank.person.Admin;
 import Bank.person.Person;
 import Bank.person.User;
 import Bank.tools.BundleFactory;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BankManagementSystem {
-    private final List<Person> users = new ArrayList<>();
-    private User user;
+    private static final List<Person> users = new ArrayList<>();
+    @Getter
+    private static User user;
     private static Bundle bundle;
 
     public static void main(String[] args) {
-        BankManagementSystem system = new BankManagementSystem();
-        system.loadUsers(system);
+        loadUsers();
 
         Scanner scanner = new Scanner(System.in);
-        bundle = BundleFactory.startingBundle(system.user);
+        bundle = BundleFactory.startingBundle(user);
         bundle.getInterpreter().getHelpMessage();
 
         while(bundle.isRunning) {
@@ -36,9 +37,9 @@ public class BankManagementSystem {
         bundle = updatedBundle;
     }
 
-    private void loadUsers(BankManagementSystem system) {
-        system.users.add(new Admin("Hans", "2/4/1985", 123999687849L, "1122", 43L));
+    private static void loadUsers() {
+        users.add(new Admin("Hans", "2/4/1985", 123999687849L, "1122", 43L));
 
-        system.user = new User(users);
+        user = new User(users);
     }
 }
