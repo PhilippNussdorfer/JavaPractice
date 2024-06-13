0416ada6d30e5a988fdc3ef9aa3e9a80c67ad703
Philipp <Philipp.nuss@gmx.at>
2024-06-13 10:22:53 +0200

    Added Exception for if the Bundle is null

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/Command.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/Command.java
@@ -1,9 +1,10 @@
 package Bank.commandInterpretor;
 
 import Bank.Bundle;
+import Bank.customExceptions.NoBundleException;
 
 public interface Command {
-    boolean canHandle(String command);
+    boolean canHandle(String command) throws NoBundleException;
     void execute(String[] params);
     String help();
     void addBundle(Bundle bundle);
--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/CommandABS.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/CommandABS.java
@@ -1,6 +1,7 @@
 package Bank.commandInterpretor;
 
 import Bank.Bundle;
+import Bank.customExceptions.NoBundleException;
 import Bank.tools.StringFormatter;
 import lombok.Getter;
 
@@ -15,8 +16,11 @@ public abstract class CommandABS implements Command {
     }
 
     @Override
-    public boolean canHandle(String command) {
+    public boolean canHandle(String command) throws NoBundleException {
         for (String listCommand : commands) {
+            if (bundle == null)
+                throw new NoBundleException("Bundle is missing please make sure it is added");
+
             if (command.equalsIgnoreCase(listCommand))
                 return true;
         }
