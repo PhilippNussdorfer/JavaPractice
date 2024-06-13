62b30ba7d38daf78be6cfdb76222e6b5dab0d1d9
Philipp <Philipp.nuss@gmx.at>
2024-06-13 09:28:48 +0200

    Started to implement the main class

--- BankmanagementsystemExercise/src/main/java/Bank/BankManagementSystem.java
+++ BankmanagementsystemExercise/src/main/java/Bank/BankManagementSystem.java
@@ -1,7 +1,39 @@
 package Bank;
 
-public class BankManagementSystem {
-    public static void main(String[] args) {
+import Bank.person.Admin;
+import Bank.person.Person;
+import Bank.person.User;
+import Bank.tools.BundleFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BankManagementSystem {
+    private final List<Person> users = new ArrayList<>();
+    private User user;
+    private static Bundle bundle;
+
+    public static void main(String[] args) {
+        BankManagementSystem system = new BankManagementSystem();
+        Scanner scanner = new Scanner(System.in);
+
+
+        bundle = BundleFactory.startingBundle(new User(system.users));
+        bundle.getInterpreter().getHelpMessage();
+
+        while(bundle.isRunning) {
+            System.out.print("> ");
+            var input = scanner.nextLine();
+            bundle.getInterpreter().interpret(input);
+        }
+    }
+
+    public static void updateBundle(Bundle updatedBundle) {
+        bundle = updatedBundle;
+    }
+
+    private void loadUsers(BankManagementSystem system) {
+        system.users.add(new Admin("Hans", "2/4/1985", 123999687849L, "1122", 43L));
     }
 }
