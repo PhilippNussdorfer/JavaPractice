84d1949d5edf5fde650f52a157187757dabffa27
Philipp <Philipp.nuss@gmx.at>
2024-06-13 11:12:52 +0200

    Added ChangeLimitCommand

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/ChangeLimitCommand.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/ChangeLimitCommand.java
@@ -1,5 +1,9 @@
 package Bank.commandInterpretor;
 
+import Bank.accounts.AccountType;
+import Bank.accounts.GiroAcc;
+import Bank.person.Customer;
+
 public class ChangeLimitCommand extends CommandABS {
 
     public ChangeLimitCommand(String ... commands) {
@@ -8,11 +12,20 @@ public class ChangeLimitCommand extends CommandABS {
 
     @Override
     public void execute(String[] params) {
-
+        try {
+            if (bundle.getSession().getUser() instanceof Customer) {
+                ((GiroAcc) ((Customer) bundle.getSession().getUser()).getAccount(AccountType.GIRO.getValue())).setLimit(Double.parseDouble(params[1]));
+            } else {
+                System.out.println("Please make sure this user is an: " + Customer.class.getSimpleName());
+            }
+        } catch (NumberFormatException exception) {
+            System.out.println(exception.getMessage() + "\nPlease enter an number for the limit.");
+        }
     }
 
     @Override
     public String help() {
-        return null;
+        return formatter.formatStringLength(75, "Changes the limit of your giro account.") + formatter.formatStringLength(50, "<Command> <limit>") + " | Commands: "
+                + formatter.concatStringArray(getCommands());
     }
 }
