64090670865426848f479699549b8f8087756fcd
Philipp <Philipp.nuss@gmx.at>
2024-06-13 09:28:13 +0200

    removed the Lists for the users and added them to an extra class and replaced user with session

--- BankmanagementsystemExercise/src/main/java/Bank/Bundle.java
+++ BankmanagementsystemExercise/src/main/java/Bank/Bundle.java
@@ -1,22 +1,19 @@
 package Bank;
 
 import Bank.commandInterpretor.Interpreter;
-import Bank.person.Admin;
-import Bank.person.Customer;
-import Bank.person.Person;
+import Bank.person.Session;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 public class Bundle {
+    public boolean isRunning = true;
     private Interpreter interpreter = null;
-    private final List<Customer> customers = new ArrayList<>();
-    private final List<Admin> admins = new ArrayList<>();
-    @Setter
-    private Person user;
+    @Getter
+    private final Session session;
+
+    public Bundle(Session session) {
+        this.session = session;
+    }
 
     public void addInterpreter(Interpreter interpreter) {
         this.interpreter = interpreter;
