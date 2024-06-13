95bc916bd484ff0a35f055cd0172a43aea855eb3
Philipp <Philipp.nuss@gmx.at>
2024-06-13 14:06:06 +0200

    added exception

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/Command.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/Command.java
@@ -1,11 +1,12 @@
 package Bank.commandInterpretor;
 
 import Bank.Bundle;
+import Bank.customExceptions.InvalidUserException;
 import Bank.customExceptions.NoBundleException;
 
 public interface Command {
     boolean canHandle(String command) throws NoBundleException;
-    void execute(String[] params);
+    void execute(String[] params) throws InvalidUserException;
     String help();
     void addBundle(Bundle bundle);
 }
