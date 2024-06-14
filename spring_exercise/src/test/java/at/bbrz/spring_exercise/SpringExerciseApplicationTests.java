170af3d401020323d676447a0eed9060af2ae13d
Philipp <Philipp.nuss@gmx.at>
2024-06-14 09:13:15 +0200

    initial commit

---
+++ spring_exercise/src/test/java/at/bbrz/spring_exercise/SpringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bbrz.spring_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
