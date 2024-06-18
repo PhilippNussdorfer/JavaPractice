ee6af8258110190297dfe096dedc745df6f89bab
Philipp <Philipp.nuss@gmx.at>
2024-06-18 10:55:46 +0200

    Refactored and changed how the login works

--- BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/LoginCommand.java
+++ BankmanagementsystemExercise/src/main/java/Bank/commandInterpretor/LoginCommand.java
@@ -1,5 +1,8 @@
 package Bank.commandInterpretor;
 
+import Bank.Bundle;
+import Bank.customExceptions.InvalidInputException;
+import Bank.customExceptions.LoginFailedException;
 import Bank.person.*;
 import Bank.tools.BundleFactory;
 
@@ -10,25 +13,35 @@ public class LoginCommand extends CommandAbstract {
     }
 
     @Override
-    public void execute(String[] params) {
-        try {
-            for (Person user : bundle.getSystem().getUser().getUsers()) {
-                if (user.login(params[2])) {
-                    System.out.println("\nYou have been logged in.");
+    public void execute(String[] params) throws InvalidInputException, LoginFailedException, NumberFormatException {
+        var last = formatter.CheckIfElementExists(2, params);
 
-                    if (user instanceof Customer)
-                        bundle.getSystem().updateBundle(BundleFactory.createCustomerBundle(new Session(user), bundle));
-                    if (user instanceof Admin)
-                        bundle.getSystem().updateBundle(BundleFactory.createAdminBundle(new Session(user), bundle));
+        for (Person user : bundle.getSystem().getUser().getUsers()) {
+            Bundle tmpBundle = null;
+            Admin admin = null;
+            Customer customer = null;
 
-                } else {
-                    System.out.println("login failed Please make sure the inputs for the ID and pin are correct.");
-                }
+            if (user instanceof Customer) {
+                customer = (Customer) user;
+                tmpBundle = BundleFactory.createCustomerBundle(new Session(user), bundle);
             }
 
-        } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
+            if (user instanceof Admin) {
+                admin = (Admin) user;
+                tmpBundle = BundleFactory.createAdminBundle(new Session(user), bundle);
+            }
+
+            if (user.login(last) && (
+                    customer != null && customer.getCustomerID() == Long.parseLong(params[1])
+                            || admin != null && admin.getEmployeeID() == Long.parseLong(params[1])
+            )) {
+                System.out.println("\nYou have been logged in.");
+                bundle.getSystem().updateBundle(tmpBundle);
+                return;
+            }
         }
+
+        throw new LoginFailedException("login failed Please make sure the inputs for the ID and pin are correct.");
     }
 
     @Override
