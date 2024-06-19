75b867316b68bdb2b15577e8b02bb41dc23dbe94
Philipp <Philipp.nuss@gmx.at>
2024-06-19 10:09:27 +0200

    Tests for the interpreter

---
+++ BankmanagementsystemExercise/src/test/java/Bank/commandInterpretor/InterpreterTest.java
@@ -0,0 +1,94 @@
+package Bank.commandInterpretor;
+
+import Bank.customExceptions.*;
+import Bank.tools.StringFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class InterpreterTest {
+
+    Interpreter interpreter;
+
+    @Mock
+    HelpCommand helpCommand;
+    @Mock
+    CommandAbstract addCustomerCommand;
+    @Mock
+    StringFormatter formatter;
+    @Mock
+    CommandAbstract loginCommand;
+    @Mock
+    CommandAbstract transferCommand;
+
+    @BeforeEach
+    void setUp() {
+        interpreter = new Interpreter(formatter);
+        interpreter.addCommand(addCustomerCommand, helpCommand, loginCommand, transferCommand);
+    }
+
+    @Test
+    void addCommand() {
+        assertEquals(4, interpreter.getCommands().size());
+        assertTrue(interpreter.getCommands().contains(helpCommand));
+        assertTrue(interpreter.getCommands().contains(addCustomerCommand));
+        assertTrue(interpreter.getCommands().contains(loginCommand));
+    }
+
+    @Test
+    void interpret() throws NoBundleException, InvalidInputException, LoginFailedException, InvalidUserException, TransferFailedException {
+        interpreter.interpret("");
+        Mockito.verify(helpCommand, Mockito.times(1)).execute(null);
+
+        Mockito.when(loginCommand.canHandle("login")).thenReturn(true);
+        interpreter.interpret("login");
+        Mockito.verify(loginCommand, Mockito.times(1)).execute(new String[] {"login"});
+    }
+
+    @Test
+    void getHelpMessage() {
+        interpreter.getHelpMessage();
+        Mockito.verify(helpCommand, Mockito.times(1)).execute(null);
+    }
+
+    @Test
+    void exceptionsOnInterpret() throws InvalidInputException, InvalidUserException, NoBundleException, LoginFailedException, TransferFailedException {
+        Mockito.when(addCustomerCommand.canHandle("add")).thenReturn(true);
+        Mockito.doThrow(new InvalidUserException("Invalid User")).when(addCustomerCommand).execute(Mockito.any(String[].class));
+        interpreter.interpret("add f, g, h, i");
+        Mockito.verify(formatter, Mockito.times(1)).outputWrapper("Invalid User");
+
+
+        Mockito.doThrow(new InvalidInputException("Invalid Input")).when(addCustomerCommand).execute(Mockito.any(String[].class));
+        interpreter.interpret("add f, g, h, i");
+        Mockito.verify(formatter, Mockito.times(1)).outputWrapper("Invalid Input");
+
+
+        Mockito.doThrow(new NumberFormatException("Not a Number")).when(addCustomerCommand).execute(Mockito.any(String[].class));
+        interpreter.interpret("add f, g, h, i");
+        Mockito.verify(formatter, Mockito.times(1)).outputWrapper("Not a Number");
+
+
+        Mockito.doThrow(new NoBundleException("No Bundle Found")).when(addCustomerCommand).canHandle(Mockito.anyString());
+        interpreter.interpret("add f, g, h, i");
+        Mockito.verify(formatter, Mockito.times(1)).outputWrapper("No Bundle Found");
+
+
+        Mockito.when(loginCommand.canHandle("login")).thenReturn(true);
+        Mockito.doThrow(new LoginFailedException("Login Failed")).when(loginCommand).execute(Mockito.any(String[].class));
+        interpreter.interpret("login f, g, h, i");
+        Mockito.verify(formatter, Mockito.times(1)).outputWrapper("Login Failed");
+
+
+        Mockito.when(transferCommand.canHandle("transfer")).thenReturn(true);
+        Mockito.doThrow(new TransferFailedException("Transfer Failed")).when(transferCommand).execute(Mockito.any(String[].class));
+        interpreter.interpret("transfer f, g, h, i");
+        Mockito.verify(formatter, Mockito.times(1)).outputWrapper("Transfer Failed");
+    }
+}
