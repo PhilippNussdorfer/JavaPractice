29ee61f9a9d3a47cd14076e6c947ad9c3b07ed5f
Philipp <Philipp.nuss@gmx.at>
2024-06-19 12:17:07 +0200

    Tests for the AddAccountCommand

---
+++ BankmanagementsystemExercise/src/test/java/Bank/commandInterpretor/AddAccountCommandTest.java
@@ -0,0 +1,71 @@
+package Bank.commandInterpretor;
+
+import Bank.Bundle;
+import Bank.accounts.Account;
+import Bank.customExceptions.*;
+import Bank.person.Admin;
+import Bank.person.Customer;
+import Bank.person.Session;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AddAccountCommandTest {
+
+    AddAccountCommand addAccountCommand = new AddAccountCommand("add");
+    @Mock
+    Bundle bundle;
+    @Mock
+    Session session;
+    @Mock
+    Admin admin;
+    @Mock
+    Customer customer;
+    @Mock
+    Account account;
+
+    @BeforeEach
+    void setUp() {
+        addAccountCommand.setBundle(bundle);
+    }
+
+    @Test
+    void execute() throws AccountTypeAlreadyExists, AccountTypeNotExisting, InvalidInputException, LoginFailedException, InvalidUserException, TransferFailedException {
+        Mockito.when(bundle.getSession()).thenReturn(session);
+        Mockito.when(session.getUser()).thenReturn(customer);
+
+        addAccountCommand.execute(new String[] {"add", "giro"});
+        Mockito.verify(customer, Mockito.times(1)).addAccount("giro");
+    }
+
+    @Test
+    void canHandle() throws NoBundleException {
+        assertTrue(addAccountCommand.canHandle("ADD"));
+        assertFalse(addAccountCommand.canHandle("exit"));
+    }
+
+    @Test
+    void exceptions() throws AccountTypeAlreadyExists, AccountTypeNotExisting {
+        Mockito.when(bundle.getSession()).thenReturn(session);
+        Mockito.when(session.getUser()).thenReturn(customer);
+        Mockito.doThrow(AccountTypeAlreadyExists.class).when(customer).addAccount("giro");
+        assertThrows(AccountTypeAlreadyExists.class, ()-> addAccountCommand.execute(new String[] {"add", "giro"}));
+
+        Mockito.doThrow(AccountTypeNotExisting.class).when(customer).addAccount("garo");
+        assertThrows(AccountTypeNotExisting.class, ()-> addAccountCommand.execute(new String[] {"add", "garo"}));
+
+        addAccountCommand.setBundle(null);
+        assertThrows(NoBundleException.class, ()-> addAccountCommand.canHandle("add"));
+    }
+
+    @Test
+    void help() {
+        assertEquals("Adds accounts that are not existing                                        <Command> <Account type>                           | Account types: giro, credit, savings | Commands: add", addAccountCommand.help());
+    }
+}
