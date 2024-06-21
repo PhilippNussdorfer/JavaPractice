bb3951a0c874152d96156beb53726cc40a7d1188
Philipp <Philipp.nuss@gmx.at>
2024-06-21 10:27:13 +0200

    refactoring

--- spring_exercise/src/main/java/at/bbrz/spring_exercise/ExerciseController.java
+++ spring_exercise/src/main/java/at/bbrz/spring_exercise/ExerciseController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExerciseController {
 
     @GetMapping(path = "/hello")
-
     public String helloWorld() {
         return "Hello World!";
     }
