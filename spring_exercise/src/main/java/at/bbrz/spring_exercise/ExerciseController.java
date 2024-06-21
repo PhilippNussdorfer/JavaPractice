7602587b9b805ceee74507c10c298863bf8a200a
Philipp <Philipp.nuss@gmx.at>
2024-06-21 10:26:19 +0200

    Initial commit

--- spring_exercise/src/main/java/at/bbrz/spring_exercise/ExerciseController.java
+++ spring_exercise/src/main/java/at/bbrz/spring_exercise/ExerciseController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExerciseController {
 
     @GetMapping(path = "/hello")
+
     public String helloWorld() {
         return "Hello World!";
     }
