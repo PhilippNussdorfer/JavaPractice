b91ebf340b7ec91953cc1ff5fcce27332d31d88e
Philipp <Philipp.nuss@gmx.at>
2024-07-17 11:25:24 +0200

    tested the TxtReader class

--- netconomy_task_server/src/main/java/server/Starter.java
+++ netconomy_task_server/src/main/java/server/Starter.java
@@ -2,26 +2,26 @@ package server;
 
 import server.encryption.suite.Encryptor;
 import server.filereader.FileReader;
-import server.filereader.XlsxReader;
+import server.filereader.TxtReader;
 import server.sqlite.SqlConnection;
 import server.sqlite.SqlConnector;
 
+import java.io.*;
+import java.net.Socket;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 
 public class Starter {
     public static void main(String[] args) throws SQLException {
-        String key = "";
+        /*String key = "";
         SqlConnection sqlConnection = new SqlConnection(SqlConnector.connect("jdbc:sqlite:", "src/main/java/server/sqlite/database.sqlite3"));
-        ResultSet res = sqlConnection.getKeyFromFile("ExternalInput.xlsx");
+        ResultSet res = sqlConnection.getKeyFromFile("ExternalInput.txt");
         while (res.next()) {
             System.out.println(key = res.getString("KEY"));
         }
-        sqlConnection.closeConnection();
+        sqlConnection.closeConnection();*/
 
-        /*try {
+        try {
             Socket socket = Server.serverStart(5000);
             if (socket == null) {
                 System.out.println("The socket is null from the server start!");
@@ -31,18 +31,23 @@ public class Starter {
             DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
-            Server server = new Server(socket, in, out);
+            new Server(socket, in, out);
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
-        }*/
+        }
 
-        FileReader<Map<Integer, List<String>>> reader = new XlsxReader();
+        /*FileReader<Map<Integer, List<String>>> reader = new XlsxReader();
         Map<Integer, List<String>> data = reader.readFile("src/main/resources/Input/ExternalInput.xlsx");
 
         for (Map.Entry<Integer, List<String>> entry : data.entrySet()) {
             for (String encrypted : entry.getValue()) {
                 System.out.println(Encryptor.decrypt(key, encrypted));
             }
-        }
+        }*/
+
+        /*FileReader<String> reader = new TxtReader();
+        String data = reader.readFile("src/main/resources/Input/ExternalInput.txt");
+
+        System.out.println(Encryptor.decrypt(key, data));*/
     }
 }
