24cfd098a3f770facbf99785341bd260c894a959
Philipp <Philipp.nuss@gmx.at>
2024-07-18 14:12:49 +0200

    refactoring

--- netconomy_task_server/src/main/java/server/Starter.java
+++ netconomy_task_server/src/main/java/server/Starter.java
@@ -9,14 +9,6 @@ public class Starter {
     private static final InputHandler inputHandler = new InputHandler();
 
     public static void main(String[] args) {
-        /*String key = "";
-        SqlConnection sqlConnection = new SqlConnection(SqlConnector.connect("jdbc:sqlite:", "src/main/java/server/sqlite/database.sqlite3"));
-        ResultSet res = sqlConnection.getKeyFromFile("ExternalInput.txt");
-        while (res.next()) {
-            System.out.println(key = res.getString("KEY"));
-        }
-        sqlConnection.closeConnection();*/
-
         try {
             int port = inputHandler.askForPort();
             String pathToDatabase = inputHandler.fileOrDirExists("Please enter the Path to the Database", "Could not find the File \n");
@@ -36,19 +28,5 @@ public class Starter {
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
-
-        /*FileReader<Map<Integer, List<String>>> reader = new XlsxReader();
-        Map<Integer, List<String>> data = reader.readFile("src/main/resources/Input/ExternalInput.xlsx");
-
-        for (Map.Entry<Integer, List<String>> entry : data.entrySet()) {
-            for (String encrypted : entry.getValue()) {
-                System.out.println(Encryptor.decrypt(key, encrypted));
-            }
-        }*/
-
-        /*FileReader<String> reader = new TxtReader();
-        String data = reader.readFile("src/main/resources/Input/ExternalInput.txt");
-
-        System.out.println(Encryptor.decrypt(key, data));*/
     }
 }
