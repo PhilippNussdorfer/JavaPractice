319a01f1f12d5a3bec7563503358b8c7c1b23cdc
Philipp <Philipp.nuss@gmx.at>
2024-07-29 09:16:16 +0200

    refactoring and moving

--- Bookmark_javaFX/src/main/java/at/bookmark/bookmark_javafx/tools/LogicCore.java
+++ Bookmark_javaFX/src/main/java/at/bookmark/bookmark_javafx/tools/LogicCore.java
@@ -1,5 +1,20 @@
 package at.bookmark.bookmark_javafx.tools;
 
-public class LogicCore {
+import at.bookmark.bookmark_javafx.GUITools.DependencyBundle;
+import at.bookmark.bookmark_javafx.GUITools.GridBuilder;
+import at.bookmark.bookmark_javafx.GUITools.Notification;
 
+public class LogicCore {
+    public void updateViewAndSearchView(DependencyBundle dB, Notification notification, GridBuilder gridBuilder, String newValue) {
+        if (newValue.equals("")) {
+            dB.getGridSearch().getChildren().clear();
+        } else {
+            var results = dB.getSearch().searchForBookmark(newValue, dB);
+
+            dB.getViewNodes().clear();
+
+            gridBuilder.setGrid(dB.getGridSearch(), results, notification, dB);
+            gridBuilder.setGrid(dB.getGridMain(), dB.getHandler().getBookmarks(), notification, dB);
+        }
+    }
 }
--- Bookmark_javaFX/src/main/java/at/bookmark/bookmark_javafx/tools/Search.java
+++ Bookmark_javaFX/src/main/java/at/bookmark/bookmark_javafx/tools/Search.java
@@ -15,25 +15,12 @@ public class Search {
     public TextField searchLogic(DependencyBundle dB, Notification notification, GridBuilder gridBuilder) {
         TextField txt_search = new TextField();
         txt_search.setPrefWidth(860);
-        txt_search.textProperty().addListener((observer, oldValue, newValue) -> updateViewAndSearchView(dB, notification, gridBuilder, newValue));
+        txt_search.textProperty().addListener((observer, oldValue, newValue) -> dB.getLogic().updateViewAndSearchView(dB, notification, gridBuilder, newValue));
 
         return txt_search;
     }
 
-    private void updateViewAndSearchView(DependencyBundle dB, Notification notification, GridBuilder gridBuilder, String newValue) {
-        if (newValue.equals("")) {
-            dB.getGridSearch().getChildren().clear();
-        } else {
-            var results = dB.getSearch().searchForBookmark(newValue, dB);
-
-            dB.getViewNodes().clear();
-
-            gridBuilder.setGrid(dB.getGridSearch(), results, notification, dB);
-            gridBuilder.setGrid(dB.getGridMain(), dB.getHandler().getBookmarks(), notification, dB);
-        }
-    }
-
-    private List<Bookmark> searchForBookmark(String input, DependencyBundle dB) {
+    public List<Bookmark> searchForBookmark(String input, DependencyBundle dB) {
         List<Bookmark> results = new ArrayList<>();
 
         for (Bookmark bookmark : dB.getHandler().getBookmarks()) {
