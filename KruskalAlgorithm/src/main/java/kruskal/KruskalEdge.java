4d0577a24053ac5f0d4324631b1c89edd823a34d
Philipp <Philipp.nuss@gmx.at>
2024-08-01 14:15:27 +0200

    Added toString

--- KruskalAlgorithm/src/main/java/kruskal/KruskalEdge.java
+++ KruskalAlgorithm/src/main/java/kruskal/KruskalEdge.java
@@ -9,4 +9,9 @@ public class KruskalEdge {
     private final int weight;
     private final KruskalNode nodeA;
     private final KruskalNode nodeB;
+
+    @Override
+    public String toString() {
+        return nodeA.getName() + " - " + weight + " - " + nodeB.getName();
+    }
 }
