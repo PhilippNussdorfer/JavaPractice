a8e9deb87b33465591405b98961c13a54dc37af6
Philipp <Philipp.nuss@gmx.at>
2024-08-01 14:18:48 +0200

    added lombok equalsAndHaseCode

--- KruskalAlgorithm/src/main/java/kruskal/KruskalNode.java
+++ KruskalAlgorithm/src/main/java/kruskal/KruskalNode.java
@@ -1,9 +1,11 @@
 package kruskal;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 public class KruskalNode {
     private final String name;
